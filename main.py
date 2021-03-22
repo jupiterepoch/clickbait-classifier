4c237ffea7d1514d4bc1ba19cc9aa5042d29bd70
Chenshu Zhu <jupiterz@umich.edu>
2021-03-22 23:29:07 +0800

    preprocessing

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import pandas as pd
 import numpy as np
 import os
 
+import torch
+import torch.nn as nn
+
+from models import Attention_Model
+
 import pdb
 
 class Webis16:
     def __init__(self):
         self.truth_path = './data/webis-clickbait-16/truth/'
         self.problem_path = './data/webis-clickbait-16/problems/'
+
     def get_truths(self, annotator="majority"):
         '''
         reads in the ground truth file and returns dictionary `ground_truth`
@@ -33,22 +39,55 @@ class Webis16:
         f.close()
 
 class Webis17:
+    class Tweet:
+        tid = None # tweet id
+        title = None
+        text = None
+        label = None
+        def __init__(self, tid, title, text):
+            self.tid = tid
+            self.title = title
+            self.text = text
+
     truth_file = None
     problem_file = None
+    tweets = {}
+
     def __init__(self, path):
         self.truth_file = path + 'truth.jsonl'
         self.problem_file = path + 'instances.jsonl'
-    def get_truths(self):
+
+    def get_truths(self, size=-1):
         df = pd.read_json(self.truth_file, lines=True)
+        df = df.loc[:size, :]
         print(df)
-        return df['id', 'truthMean'].to_numpy()
+        return df['id'], df['truthMean'].values
+
+    def get_texts(self, size=-1):
+        df = pd.read_json(self.problem_file, lines=True)
+        df = df.loc[:size, :]
+        print(df)
+        print(df.columns)
+        # postText are always len=1 list
+        for t in df:
+            self.tweets[t['id']] = Tweet(t['id'], t['targetTitle'].values[0], t['targetParagraphs'].values[0])
+        # return df['id'], df['targetTitle'].values[0], df['targetParagraphs'].values[0]
+
+    def preprocessing(self):
+        from transformers import BertTokenizer
+        for t in self.tweets:
+            print(t.title)
+
 
 def main():
-    web16 = Webis16()
-    # web16.get_tweet_content()
     web17 = Webis17('./data/clickbait17/')
-    ground_truth = web17.get_truths()
-    print(ground_truth.shape)
+    size = 10
+    # TODO: tweet id is a int64, check for overflow!
+    (truth_id, label) = web17.get_truths(size)
+    web17.preprocessing()
+    #(tweet_id, titles, texts) = web17.get_texts(size)
+    #print(titles[0])
+    #print(texts[0])
 
 if __name__ == '__main__':
     main()
---
+++ models.py
@@ -0,0 +1,24 @@
+class Attention_Model(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers): # , batch_size
+        super(Attention_Model, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.rnn = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
+        # self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.dense = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.Tanh(),
+            nn.Linear(hidden_size, 1)
+        )
+        self.linear = nn.Linear(hidden_size, 1)
+        self.cos = nn.CosineSimilarity()
+        
+    def forward(self, input1, input2, init_hc):
+        # input 1 and input 2 are already embeddings
+        out1, (hn1, cn1) = self.rnn(input1, init_hc)
+        out2, (hn2, cn2) = self.rnn(input2, init_hc)
+        out1 = self.linear(hn1).squeeze().transpose(0, 1)
+        out2 = self.linear(hn2).squeeze().transpose(0, 1)
+        #out1 = self.dense(input1).squeeze()
+        #out2 = self.dense(input2).squeeze()
+        return 1 + 4 * self.cos(out1, out2)
